cb553f36429f1d0fa1e4f9d9f3a3bbc14e784173
LiKang <likang@cpgroup.cn>
2021-11-02 11:28:12 +0800

    perf: add code

--- Controller/UserController.go
+++ Controller/UserController.go
@@ -8,6 +8,7 @@ import (
 type UserInfo struct {
 	UserName string `json:"user_name"`
 	Password string `json:"password"`
+	Address  string `json:"address"`
 }
 
 func Login(context *gin.Context) {
@@ -18,9 +19,10 @@ func Login(context *gin.Context) {
 	var user1, user2 UserInfo
 	user1.UserName = "zhangsan"
 	user1.Password = "123456"
+	user1.Address = "北京"
 	user2.UserName = "lisi"
 	user2.Password = "1234567"
-	arr := [...]UserInfo{user1, user2}
+	arr := [...]UserInfo{user1, user2, userInfo}
 	SuccessResp(context, "success", arr)
 	return
 }
